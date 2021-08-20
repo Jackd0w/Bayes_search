8a0a5099c853b9d5d86d3323d51df253a54e3de8
laughing man <komarovandrey396@gmail.com>
2021-08-20 15:40:43 +0300

    added map

---
+++ bayes.py
@@ -0,0 +1,10 @@
+import sys
+import itertools
+import random
+import numpy as np
+import cv2 as cv
+
+MAP_FILE = 'insert png here'
+SA1_CORNER = (130, 265, 180, 315)
+SA2_CORNER = (80, 255, 130, 305)
+SA3_CORNER = (105, 205, 155, 255)
