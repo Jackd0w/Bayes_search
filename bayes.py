a988d61b470899be9ace51b128d247121d6ee668
laughing man <komarovandrey396@gmail.com>
2021-08-20 22:23:41 +0300

    added method to draw map

--- bayes.py
+++ bayes.py
@@ -22,3 +22,33 @@ class Search():
         self.area_actual = 0
         self.sailor_actual = [0, 0] #local coordinates search area
         
+        self.sa1 = self.img[SA1_CORNERS[1] : SA1_CORNERS[3],
+                            SA1_CORNERS[0] : SA1_CORNERS[4]]
+        self.sa2 = self.img[SA2_CORNERS[1] : SA2_CORNERS[3],
+                            SA2_CORNERS[0] : SA2_CORNERS[4]]
+        self.sa3 = self.img[SA3_CORNERS[1] : SA3_CORNERS[3],
+                            SA3_CORNERS[0] : SA3_CORNERS[4]]
+
+        self.p1 = 0.2
+        self.p2 = 0.5
+        self.p3 = 0.3
+
+        self.sep1 = 0
+        self.sep2 = 0
+        self.sep3 = 0
+
+
+
+    def draw_map(self, last_known):
+        #Display fullskale map with last knonw position marker
+        cv.line(self.img, (20, 370), (70, 370), (0, 0, 0), 2)
+        cv.putText(self.img, '0', (8, 370), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+        cv.putText(self.img, '50 Nautical Miles ', (71, 370), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+
+        cv.rectangle(self.img, (SA1_CORNERS[1], SA1_CORNERS[3],
+                                SA1_CORNERS[0], SA1_CORNERS[4]), (0, 0, 0), 1)
+        cv.putText(self.img, '1', (SA1_CORNERS[0]+3), SA1_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
+        cv.rectangle(self.ingm (SA2_CORNERS[0], SA2_CORNERS[1]), (SA2_CORNERS[2], SA2_CORNERS[3]), (0, 0, 0), 1)
+        cv.putText(self.img, '2', (SA2_CORNERS[0] + 3, SA2_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
+        cv.rectangle(self.img, (SA3_CORNERS[0], SA3_CORNERS[1]), (SA3_CORNERS[2], SA3_CORNERS[3]), (0, 0, 0), 1)
+        cv.putText(self.img, '3', (SA3_CORNERS[0] + 3, SA3_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
