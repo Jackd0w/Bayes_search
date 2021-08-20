d8c3ef175bc67dcd118ff0444b027c00e29938bc
laughing man <komarovandrey396@gmail.com>
2021-08-20 17:55:14 +0300

    search class added

--- bayes.py
+++ bayes.py
@@ -4,7 +4,21 @@ import random
 import numpy as np
 import cv2 as cv
 
-MAP_FILE = 'insert png here'
+MAP_FILE = 'map.png'
 SA1_CORNER = (130, 265, 180, 315)
 SA2_CORNER = (80, 255, 130, 305)
 SA3_CORNER = (105, 205, 155, 255)
+
+
+class Search():
+
+    def __init__(self, name):
+        self.name = name
+        self.img = cv.imread(MAP_FILE, cv.IMREAD_COLOR)
+        if self.img is None:
+            print('Error! Could not open map file {}'.format(MAP_FILE), file=sys.stderr)
+            sys.exit(1)
+
+        self.area_actual = 0
+        self.sailor_actual = [0, 0] #local coordinates search area
+        
